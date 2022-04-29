a041f8b68422834f05c221fa3347df13d5642cdb
DannyLenk <danylenko.14@gmail.com>
2022-04-29 11:14:16 +0300

    added basic features

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from "react"
 import Die from "./Die"
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 
 export default function App() {
 
     const [dice, setDice] = React.useState(createAllDice())
+    const [tenzies, setTenzies] = React.useState(false)
+
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const allEqual = dice.every((die, i, arr) => arr[0].value === die.value )
+        if (allHeld && allEqual) {
+            setTenzies(true)
+        }
+    }, [dice])
+
 
     function createDie() {
         return {
@@ -28,6 +39,7 @@ export default function App() {
             die.id === id
                 ? {...die, isHeld: !die.isHeld}
                 : die
+
         )))
     }
 
@@ -40,14 +52,30 @@ export default function App() {
     ))
 
     function rollDice() {
-        setDice(prevState => prevState.map(die => (
-            die.isHeld ? die : createDie()
-        )))
+
+        if (!tenzies) {
+
+            setDice(prevState => prevState.map(die => (
+                die.isHeld ? die : createDie()
+            )))
+
+        } else {
+            setDice(createAllDice())
+            setTenzies(false)
+        }
     }
 
 
     return(
         <main>
+
+            {tenzies && <Confetti />}
+
+            <h1 className="title">Tenzies</h1>
+
+            <h3 className="instructions">
+                Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+            </h3>
 
             <section className="dice-container">
                 {allDice}
@@ -56,7 +84,7 @@ export default function App() {
             <button className="roll-dice"
                 onClick={rollDice}
             >
-                Roll
+                {!tenzies ? 'Roll' : 'New Game'}
             </button>
         </main>
     )
